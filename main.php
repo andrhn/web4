8df2981571be5ee3a5b5d39f2de683b401ccef03
Katherine Andrhn <katherine.andrhn@gmail.com>
2019-04-29 12:33:36 +0300

    display rows from db

--- main.php
+++ main.php
@@ -13,10 +13,11 @@
 <?php>
 $user = 'root';
   $password = 'root';
-  $db = 'news_db';
+  $db = 'test';
   $host = 'localhost';
   $port = 3307;
   $link = mysqli_init();
+
   $conn = mysqli_real_connect(
     $link, 
     $host, 
@@ -26,25 +27,25 @@ $user = 'root';
     $port
   );
 
-  if (!$link) {
+  if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
   } 
 
-?>
+
+$sql = ("SELECT id, author, date, title, context FROM newsnews");
+$result = mysqli_query($link, $sql);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+if (mysqli_num_rows($result)> 0){
+	while($row = mysqli_fetch_assoc($result)) {
+	?>  
+    <div class="news_block">
+      <div class="news_title"> 
+      </div>
+         <b><? echo $row["author"], " ", $row["date"]; ?></b>
+      <p><? echo $row["context"]; ?></p>
+    <? } }?>
+    </div>
 
   </body>
 </html>
